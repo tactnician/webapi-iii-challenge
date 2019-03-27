0d4d271cee48712a3e99003c7761f4fe1fb9a7ee
tactnician <tactnician@gmail.com>
2019-03-27 19:16:19 -0400

    all crud operations in post-router complete

--- posts/post-router.js
+++ posts/post-router.js
@@ -43,10 +43,44 @@ router.post('/', async (req, res) => {
 });
 
 //update()
-// router.put();
+router.put('/:id', async (req, res) => {
+    try {
+        const post = await Posts.update(req.params.id, req.params.text);
+        console.log("Post to be changed", post)
+        if(post){
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({
+                message: `post could not be found`
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: `error updating post ${error}`
+        })
+    }
+    
+});
 
 //remove()
-// router.delete();
+router.delete('/:id', async (req, res) => {
+    try {
+        const count = await Posts.remove(req.params.id);
+        if(count > 0){
+            res.status(200).json({
+                message: `post has been deleted`
+            })
+        } else {
+            res.status(404).json({
+                message: `post could not be found`
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message:`error removing post ${error}`
+        })
+    }
+});
 
 
 module.exports = router;
